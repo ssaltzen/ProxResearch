5511b3683c8420c8154287875dc4e9d019a7a3d9
jotyot <104177975+jotyot@users.noreply.github.com>
2023-05-27 18:42:58 -0700

    Player Animated

--- Social Proxima Testing/Assets/Scripts/PlayerController.cs
+++ Social Proxima Testing/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ namespace Proxemics
 {
     public class PlayerController : MonoBehaviour
     {
+        [SerializeField] private Animator animator;
         private CharacterController controller;
         private Vector3 playerVelocity;
         private bool groundedPlayer;
@@ -24,19 +25,25 @@ namespace Proxemics
 
         private void Update()
         {
-            MoveUpdate();
+            Move();
         }
 
-        private void MoveUpdate()
+        private void Move()
         {
             groundedPlayer = controller.isGrounded;
             if (groundedPlayer && playerVelocity.y < 0)
             {
                 playerVelocity.y = 0f;
             }
-
+            animator.SetBool("Jump", !groundedPlayer);
+            
             // Pretty much stolen from CharacterController Move documention.
             Vector3 move = new Vector3(moveDirection.x, 0, moveDirection.y);
+
+            // The ternery operator is for backwards diagonal movement.
+            animator.SetFloat("Horizontal", moveDirection.x * (moveDirection.y >= 0 ? 1 : -1));
+            animator.SetFloat("Vertical", moveDirection.y);
+
             // So player moves in direction of the camera.
             move = cameraTransform.forward * move.z + cameraTransform.right * move.x;
             move.y = 0f;
@@ -44,6 +51,9 @@ namespace Proxemics
 
             playerVelocity.y += gravityValue * Time.deltaTime;
             controller.Move(playerVelocity * Time.deltaTime);
+
+            // Rotate model based on where the player is looking.
+            transform.rotation = Quaternion.Euler(0, cameraTransform.rotation.eulerAngles.y, 0);
         }
 
         // From Player Input component.
