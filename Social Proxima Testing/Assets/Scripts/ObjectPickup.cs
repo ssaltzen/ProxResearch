7925af64633ef0efaa61834abfed192ce23e6aa0
dylanteehankee <dylanteehankee@gmail.com>
2023-06-01 14:03:24 -0700

    ObjectPickup
    Added ObjectPickup script and added tags for each interactable object.

---
+++ Social Proxima Testing/Assets/Scripts/ObjectPickup.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+
+public class ObjectPickup : MonoBehaviour
+{
+    private GameObject pickedObject;
+    private Transform originalParent;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (pickedObject == null)
+            {
+                PickupObject();
+            }
+            else
+            {
+                ReleaseObject();
+            }
+        }
+    }
+
+    private void PickupObject()
+    {
+        // Perform a raycast to detect an interactable object
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider.CompareTag("Interactable"))
+            {
+                // Store a reference to the picked object
+                pickedObject = hit.collider.gameObject;
+
+                // Disable physics simulation for the picked object
+                Rigidbody rb = pickedObject.GetComponent<Rigidbody>();
+                if (rb != null)
+                {
+                    rb.isKinematic = true;
+                }
+
+                // Store the original parent of the picked object
+                originalParent = pickedObject.transform.parent;
+
+                // Attach the picked object to the player's hand or attachment point
+                pickedObject.transform.SetParent(transform);
+            }
+        }
+    }
+
+    private void ReleaseObject()
+    {
+        // Restore the original parent of the picked object
+        pickedObject.transform.SetParent(originalParent);
+
+        // Enable physics simulation for the picked object
+        Rigidbody rb = pickedObject.GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.isKinematic = false;
+        }
+
+        // Reset the reference to the picked object
+        pickedObject = null;
+    }
+}
