cdcafda0aad87cb7b763e505559c5a5aed214206
btthl <davin.nguyen19@gmail.com>
2023-06-01 18:38:50 -0700

    added face tracking script for virtual models

--- Social Proxima Testing/Assets/VirtualModels/Scripts/FaceTracking.cs
+++ Social Proxima Testing/Assets/VirtualModels/Scripts/FaceTracking.cs
@@ -5,38 +5,75 @@ using UnityEngine;
 // Makes virtual model look at the player within a certain range
 public class FaceTracking : MonoBehaviour
 {
-    [SerializeField]
-    [Range(0f, 90f)]
-    private float maxHorizontalNeckTurn;
+    private const float followSpeed = 3.0f;
 
     [SerializeField]
     [Range(0f, 90f)]
-    private float maxVerticalNeckTurn;
-
+    private float maxHeadTurn;
 
     [SerializeField]
-    private Transform neckBone;
     private Transform headBone;
 
     [SerializeField]
     private Transform objectTracking;
 
+    private float trackingGradient = 0f;                // For smooth transition from head position in animation
+    private Vector3 lastRealDirection = Vector3.zero;   // Saves last direction set by script to transition back to animation when focus is lost
 
-    // Start is called before the first frame update
-    void Start()
+
+    void LateUpdate()
     {
+        // Do nothing if there is no object
+        if (!objectTracking)
+        {
+            ReturnHead();
+            return;
+        }
         
+        Vector3 targetDirection = GetTargetHeadDirection();
+
+        if (Vector3.Angle(headBone.forward, targetDirection) > maxHeadTurn)
+        {
+            ReturnHead();
+            return;
+        }
+
+        UpdateTrackingGradient(true);
+
+        Vector3 realHeadDirection = Vector3.Lerp(headBone.forward, targetDirection, trackingGradient);
+        headBone.forward = realHeadDirection;
+
+        lastRealDirection = realHeadDirection;
     }
 
-    // Update is called once per frame
-    void Update()
+    // Transitions from the last direction set by the script to the animation
+    private void ReturnHead()
     {
-        
+        UpdateTrackingGradient(false);
+        Vector3 realHeadDirection = Vector3.Lerp(headBone.forward, lastRealDirection, trackingGradient);
+        headBone.forward = realHeadDirection;
     }
 
-    private float GetAngleTowardsTarget()
+    // Updates tracking gradient based on whether the target can be focused on or not
+    private void UpdateTrackingGradient(bool isTracking)
     {
-        return 0f;
+        if (isTracking)
+        {
+            trackingGradient = Mathf.Min(trackingGradient + Time.deltaTime * followSpeed, 1);
+        }
+        else
+        {
+            trackingGradient = Mathf.Max(trackingGradient - Time.deltaTime * followSpeed, 0);
+        }
+    }
+
+    // Gets direction required to look at tracking object
+    private Vector3 GetTargetHeadDirection()
+    {
+        Vector3 pos = headBone.position;
+        Vector3 targetPos = objectTracking.position;
+        Vector3 difference = targetPos - pos;
+        return Vector3.Normalize(difference);
     }
 
 }
