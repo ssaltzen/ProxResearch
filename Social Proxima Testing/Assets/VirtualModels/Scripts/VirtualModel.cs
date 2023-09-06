3c8b3af17d9c5848c22168a3fe1ceca07b890031
btthl <davin.nguyen19@gmail.com>
2023-09-06 15:44:41 -0700

    Virtual model now responds to player emotes

--- Social Proxima Testing/Assets/VirtualModels/Scripts/VirtualModel.cs
+++ Social Proxima Testing/Assets/VirtualModels/Scripts/VirtualModel.cs
@@ -2,17 +2,57 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class VirtualModel : MonoBehaviour
+namespace Proxemics
 {
-    // Start is called before the first frame update
-    void Start()
+    public class VirtualModel : MonoBehaviour
     {
-        
+        private PlayerController player;
+        private Animator animator;
+
+        private Emote lastPlayerEmote;
+        private float secondsSincePlayerEmote = 0;
+        private float responseDelay = 1;
+
+        private void Awake()
+        {
+            animator = GetComponent<Animator>();
+        }
+
+        public void Init(PlayerController player)
+        {
+            this.player = player;
+            player.Emoted += OnPlayerEmoted;
+        }
+
+        private void Update()
+        {
+            if (!lastPlayerEmote)
+            {
+                return;
+            }
+
+            secondsSincePlayerEmote += Time.deltaTime;
+            if (secondsSincePlayerEmote >= responseDelay)
+            {
+                animator.SetTrigger(lastPlayerEmote.name);
+                lastPlayerEmote = null;
+            }
+        }
+
+        private void OnDisable()
+        {
+            if (player != null)
+            {
+                player.Emoted -= OnPlayerEmoted;
+            }
+        }
+
+        private void OnPlayerEmoted(Emote playerEmote)
+        {
+            lastPlayerEmote = playerEmote;
+            secondsSincePlayerEmote = 0;
+            responseDelay = Random.Range(0.25f, 1f);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
